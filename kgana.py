c246393cdc1380933b1b573d0295d59bd6479826
mennovf <mennovanfrachem@hotmail.com>
2024-08-06 19:25:46 +0200

    Transition to Textual

--- kgana.py
+++ kgana.py
@@ -1,8 +1,7 @@
 import itertools, random, os
-from rich.console import Console
-from rich.text import Text
-from rich.prompt import Prompt
-from rich.panel import Panel
+from textual.app import App
+from textual.containers import Container
+from textual.widgets import Label
 
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 CONSONANTS = ['k', 'r', 's', 't', 'm', 'n', 'h', 'y', 'w']
@@ -10,7 +9,19 @@ CONSONANTS = ['k', 'r', 's', 't', 'm', 'n', 'h', 'y', 'w']
 ALL = list(itertools.chain(map(lambda x: x[0] + x[1], itertools.product(CONSONANTS, VOWELS)), ['n']))
 random.shuffle(ALL)
 
-console = Console()
+class Screen(App):
+    CSS = """
+    Screen {
+    align: center middle;
+    }
+    """
+
+    def compose(self) -> Container:
+        yield Label("Test")
+
+app = Screen()
+app.run()
+
 for i in ALL:
     console.print(Panel(i, style="bold magenta"))
 
